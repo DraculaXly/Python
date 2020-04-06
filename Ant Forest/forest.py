027acc13593fcf627e52284d0ae2272ac1a79dc2
ly Xu <dracula_xly@outlook.com>
2020-04-06 12:03:23 +0800

    Add ant forest

---
+++ Ant Forest/forest.py
@@ -0,0 +1,74 @@
+import time
+from appium import webdriver
+from appium.webdriver.common.touch_action import TouchAction
+
+app_json = {
+            "platformName": "Android",
+            "deviceName": "", # input your deviceName, you can get it with adb
+            "appPackage": "com.eg.android.AlipayGphone",
+            "appActivity": "com.eg.android.AlipayGphone.AlipayLogin",
+            "noReset": "true",
+            "fullReset": "false",
+            "automationName": "UiAutomator1" # delete this if fail to work
+            }
+
+server = "http://localhost:4723/wd/hub"
+driver = webdriver.Remote(server, app_json)
+time.sleep(2)
+driver.find_element_by_xpath("//*[@text='Ant Forest']").click()
+time.sleep(2)
+
+# get self energy
+def get_own(driver):
+    items = driver.find_elements_by_class_name("android.widget.Button")
+    if len(items) > 5:
+        for i in items:
+            if '能量' in i.text:
+                i.click()
+                time.sleep(0.5)
+    driver.tap([(21,178), (133,290)], 200)
+
+# get friends' energy
+def get_friends(driver):
+    # swipe down to click more friends
+    driver.swipe(720, 2800, 720, 800)
+    driver.swipe(720, 2800, 720, 800)
+    driver.find_element_by_xpath("//*[@text='View more friends']").click()
+    time.sleep(1)
+
+    # get friends energy o(*￣▽￣*)ブ
+    while True:
+        TouchAction(driver).press(x=235, y=935).release().perform()
+        time.sleep(1)
+        last_f = driver.find_element_by_id("com.alipay.mobile.nebula:id/h5_tv_title")
+        if not last_f.text == '蚂蚁森林':
+            items = driver.find_elements_by_class_name("android.widget.Button")
+            if len(items) > 5:
+                for i in items:
+                    if '能量' in i.text:
+                        i.click()
+                        time.sleep(0.5)
+            # get last one's and quit
+            if last_f.text == "": #put the last friend's name such as: xxx's ant forest
+                driver.tap([(21,178), (133,290)], 200)
+                driver.tap([(21,178), (133,290)], 200)
+                time.sleep(0.2)
+                driver.swipe(720, 800, 720, 2800)
+                driver.swipe(720, 800, 720, 2800)
+                return
+            driver.tap([(21,178), (133,290)], 200)
+            time.sleep(0.5)
+            driver.swipe(157, 1126, 157, 874)
+            time.sleep(0.2)
+        else:
+            sp_items = driver.find_elements_by_class_name("android.widget.Button")
+            if len(sp_items) > 1:
+                driver.tap([(21,178), (133,290)], 200)
+                time.sleep(0.5)
+            driver.swipe(157, 1126, 157, 874)
+            time.sleep(0.2)
+
+if __name__ == "__main__":
+    get_own(driver)
+    get_friends(driver)
+    driver.quit()
--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # All Python Projects
 ## 1. Spider
 ### 一些自己写的爬虫项目
+---
 ## 2. Wheel
 ### 主要记录一些源代码编译的Wheel
+---
+## 3. Ant Forest
+### 安卓手机Alipay蚂蚁森林自动收取能量
+Tools (windows用户):  
+>Nodejs: Download [Nodejs](https://nodejs.org/en/)  
+Appium: Download [Appium](http://appium.io/)  
+JDK: Download [JDK](https://www.oracle.com/java/technologies/javase-downloads.html)  
+Android SDK:Download [Studio](https://developer.android.google.cn/studio?hl=zh-cn)，可以在软件内配置SDK，当然了，你也可以自己直接下SDK  
+Python安装库: pip install Appium-Python-Client  
+环境配置的话网上搜一下吧，我主要参考知乎——[Appium环境搭建超详细教程](https://zhuanlan.zhihu.com/p/49193525)以及Appium的github[在 Windows 上运行 Appium](https://github.com/appium/appium/blob/master/docs/cn/appium-setup/running-on-windows.md)   
+---
